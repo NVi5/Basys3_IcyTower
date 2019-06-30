429b13c6977a0ae495b5466c2b591ba31ba6f620
NVi5 <nvader19@gmail.com>
2019-06-30 03:11:53 +0200

    First working version

---
+++ src/Hello/Debug/src/helloworld.d
@@ -0,0 +1,18 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../Hello_bsp/microblaze_0/include/xil_printf.h \
+ ../../Hello_bsp/microblaze_0/include/xil_types.h \
+ ../../Hello_bsp/microblaze_0/include/xparameters.h \
+ ../../Hello_bsp/microblaze_0/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../Hello_bsp/microblaze_0/include/xil_printf.h:
+
+../../Hello_bsp/microblaze_0/include/xil_types.h:
+
+../../Hello_bsp/microblaze_0/include/xparameters.h:
+
+../../Hello_bsp/microblaze_0/include/bspconfig.h:
