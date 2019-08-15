e5007c3e4c0082fb16611f728fdf9ef1aaaa3adf
wojtek2407 <wojtek2407972@gmail.com>
2019-08-15 09:29:27 +0200

    fixed bugs in fpga interface

--- sdk/klocki_test/Debug/src/main.d
+++ sdk/klocki_test/Debug/src/main.d
@@ -1,15 +1,13 @@
-src/main.o src/main.o: ../src/main.cc ../src/../include/game.hpp \
- ../src/../include/line2d.hpp ../src/../include/point2d.hpp \
- ../src/../include/utils.h \
+src/main.o src/main.o: ../src/main.cc ../src/../include/fpga_interface.h \
+ ../src/../include/game.hpp ../src/../include/line2d.hpp \
+ ../src/../include/point2d.hpp ../src/../include/utils.h \
  ../../basys3_game_bsp/microblaze_0/include/mb_interface.h \
  ../../basys3_game_bsp/microblaze_0/include/xil_types.h \
  ../../basys3_game_bsp/microblaze_0/include/xil_assert.h \
  ../../basys3_game_bsp/microblaze_0/include/xil_exception.h \
- ../src/../include/player.hpp ../src/../include/textures.h \
- ../src/../include/fpga_interface.h \
- ../../basys3_game_bsp/microblaze_0/include/xil_printf.h \
- ../../basys3_game_bsp/microblaze_0/include/xparameters.h \
- ../../basys3_game_bsp/microblaze_0/include/bspconfig.h
+ ../src/../include/player.hpp ../src/../include/textures.h
+
+../src/../include/fpga_interface.h:
 
 ../src/../include/game.hpp:
 
@@ -30,11 +28,3 @@ src/main.o src/main.o: ../src/main.cc ../src/../include/game.hpp \
 ../src/../include/player.hpp:
 
 ../src/../include/textures.h:
-
-../src/../include/fpga_interface.h:
-
-../../basys3_game_bsp/microblaze_0/include/xil_printf.h:
-
-../../basys3_game_bsp/microblaze_0/include/xparameters.h:
-
-../../basys3_game_bsp/microblaze_0/include/bspconfig.h:
